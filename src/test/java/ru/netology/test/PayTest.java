0737234363b37e93a0cb6fc6b410b27fdb34161c
Краснова Татьяна <redtanya@yandex.ru>
2022-10-05 11:41:56 +0300

    корректные месяц и год

--- src/test/java/ru/netology/test/PayTest.java
+++ src/test/java/ru/netology/test/PayTest.java
@@ -1,16 +1,20 @@
 package ru.netology.test;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
-
+import io.qameta.allure.selenide.AllureSelenide;
 import lombok.val;
 import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.data.DataHelper;
 import ru.netology.page.PaymentPage;
 import ru.netology.page.StartPage;
 
+import java.time.Duration;
+
 import static com.codeborne.selenide.Selenide.open;
+import static java.time.Duration.ofSeconds;
 
 public class PayTest {
     StartPage startPage = new StartPage();
@@ -20,6 +24,10 @@ public class PayTest {
     @BeforeEach
     void setUp() {
         open("http://localhost:8080");
+    }
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
     }
 
 
@@ -33,11 +41,11 @@ public class PayTest {
     void shouldBuyTourWithDebetApprovedCard() {
         startPage.openPaymentPage();
         val cardNumber = DataHelper.getApprovedCardNumber();
-        val month = DataHelper.getRandomMonth();
-        val year = DataHelper.getRandomYear();
+        val month = DataHelper.getMonth;
+        val year = DataHelper.getYear;
         val owner = DataHelper.getRandomOwner();
         val cvc = DataHelper.getRandomCVC();
-        paymentPage.putCardData(cardNumber,month,year,owner,cvc);
+        paymentPage.putCardData(cardNumber, month, year, owner, cvc);
         DataHelper.getApprovedCardStatus();
 
     }
